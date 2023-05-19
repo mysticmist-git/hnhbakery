b73fae3bcb3ccc1c14002bd7faff8cf3ac001c5b
Nguyen Van Hen <89691660+String1502@users.noreply.github.com>
2023-05-20 00:04:40 +0700

    update on table rows

--- src/components/Manage/tables/components/GeneratedTableHead.tsx
+++ src/components/Manage/tables/components/GeneratedTableHead.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useContext } from 'react';
+import React, { memo, useContext, useMemo } from 'react';
 import { TableCell } from '@mui/material';
 import { CollectionName } from '@/lib/models/utilities';
 import { ManageContext } from '@/pages/manager/manage';
@@ -7,43 +7,47 @@ import { ManageContextType } from '@/lib/localLib/manage';
 const GeneratedTableHead = () => {
   const { state } = useContext<ManageContextType>(ManageContext);
 
-  switch (state.selectedTarget?.collectionName) {
-    case CollectionName.ProductTypes:
-      return (
-        <>
-          <TableCell>STT</TableCell>
-          <TableCell width={150}>Tên sản phẩm</TableCell>
-          <TableCell>Mô tả</TableCell>
-          <TableCell width={150}>Trạng thái</TableCell>
-          <TableCell align="center">Hành động</TableCell>
-        </>
-      );
-    case CollectionName.Products:
-      return (
-        <>
-          <TableCell>STT</TableCell>
-          <TableCell width={150}>Tên sản phẩm</TableCell>
-          <TableCell>Loại sản phẩm</TableCell>
-          <TableCell>Mô tả</TableCell>
-          <TableCell width={150}>Trạng thái</TableCell>
-          <TableCell align="center">Hành động</TableCell>
-        </>
-      );
-    case CollectionName.Batches:
-      return (
-        <>
-          <TableCell align="center">STT</TableCell>
-          <TableCell align="center">Sản phẩm</TableCell>
-          <TableCell align="center">Đã bán</TableCell>
-          <TableCell align="center">Tổng</TableCell>
-          <TableCell align="center">Ngày sản xuất</TableCell>
-          <TableCell align="center">Ngày hết hạn</TableCell>
-          <TableCell align="center">Hành động</TableCell>
-        </>
-      );
-    default:
-      return <div>Error generating header</div>;
-  }
+  const TableHead = useMemo(() => {
+    switch (state.selectedTarget?.collectionName) {
+      case CollectionName.ProductTypes:
+        return (
+          <>
+            <TableCell>STT</TableCell>
+            <TableCell width={150}>Tên sản phẩm</TableCell>
+            <TableCell>Mô tả</TableCell>
+            <TableCell width={150}>Trạng thái</TableCell>
+            <TableCell align="center">Hành động</TableCell>
+          </>
+        );
+      case CollectionName.Products:
+        return (
+          <>
+            <TableCell>STT</TableCell>
+            <TableCell width={150}>Tên sản phẩm</TableCell>
+            <TableCell>Loại sản phẩm</TableCell>
+            <TableCell>Mô tả</TableCell>
+            <TableCell width={150}>Trạng thái</TableCell>
+            <TableCell align="center">Hành động</TableCell>
+          </>
+        );
+      case CollectionName.Batches:
+        return (
+          <>
+            <TableCell align="center">STT</TableCell>
+            <TableCell align="center">Sản phẩm</TableCell>
+            <TableCell align="center">Đã bán</TableCell>
+            <TableCell align="center">Tổng</TableCell>
+            <TableCell align="center">Ngày sản xuất</TableCell>
+            <TableCell align="center">Ngày hết hạn</TableCell>
+            <TableCell align="center">Hành động</TableCell>
+          </>
+        );
+      default:
+        return <div>Error generating header</div>;
+    }
+  }, [state.selectedTarget?.collectionName]);
+
+  return TableHead;
 };
 
 export default memo(GeneratedTableHead);
