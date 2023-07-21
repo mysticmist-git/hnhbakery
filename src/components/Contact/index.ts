79fbf7f2466989425a1e9cd09d9680b5857631a7
Nguyen Van Hen <89691660+String1502@users.noreply.github.com>
2023-07-21 15:20:13 +0700

    refactor contact wrapper

--- src/components/Contact/index.ts
+++ src/components/Contact/index.ts
@@ -1,2 +1,2 @@
 export { default as ContactForm } from './ContactForm';
-export { default as ContactWrapper } from './ContactWrapper';
+export { default as ContactWrapper } from './ContactWrapper/ContactWrapper';
