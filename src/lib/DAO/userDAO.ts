2e7a02fabfa380b3a20f46e5ee80f97547600837
Nguyen Van Hen <89691660+String1502@users.noreply.github.com>
2023-09-21 14:03:43 +0700

    minor fix on userDAO

--- src/lib/DAO/userDAO.ts
+++ src/lib/DAO/userDAO.ts
@@ -14,23 +14,24 @@ import {
 import { COLLECTION_NAME } from '../constants';
 import {
   DEFAULT_GROUP_ID,
+  getGroupById,
   getGroupRefById,
   getGroupSnapshots,
 } from './groupDAO';
 
-export function getUsersRefByGroup(groupId: string) {
+export function getUsersRef(groupId: string) {
   return collection(
     getGroupRefById(groupId),
     COLLECTION_NAME.USERS
   ).withConverter(userConverter);
 }
 
-export function getUsersByGroupWithQuery(
+export function getUsersRefWithQuery(
   groupId: string,
   ...queryConstraints: QueryConstraint[]
 ) {
   return getDocs(
-    query(getUsersRefByGroup(groupId), ...queryConstraints).withConverter(
+    query(getUsersRef(groupId), ...queryConstraints).withConverter(
       userConverter
     )
   );
@@ -44,11 +45,11 @@ export async function queryUsersByGroup(
   groupId: string,
   ...queryConstraints: QueryConstraint[]
 ) {
-  return await getDocs(query(getUsersRefByGroup(groupId), ...queryConstraints));
+  return await getDocs(query(getUsersRef(groupId), ...queryConstraints));
 }
 
 export async function getUsersSnapshotByGroup(groupId: string) {
-  return await getDocs(getUsersRefByGroup(groupId));
+  return await getDocs(getUsersRef(groupId));
 }
 
 export async function getUsersByGroup(groupId: string) {
@@ -75,7 +76,7 @@ export async function getUserByUid(uid: string) {
   const groupsSnapshot = await getGroupSnapshots();
 
   for (let groupSnapshot of groupsSnapshot.docs) {
-    const matchUsers = await getUsersByGroupWithQuery(groupSnapshot.id);
+    const matchUsers = await getUsersRefWithQuery(groupSnapshot.id);
 
     if (matchUsers.empty) continue;
 
@@ -85,56 +86,18 @@ export async function getUserByUid(uid: string) {
   }
 }
 
-export async function updateUser(id: string, data: User, groupId?: string) {
-  try {
-    if (groupId) {
-      await updateDoc(
-        doc(
-          db,
-          COLLECTION_NAME.GROUPS,
-          groupId,
-          COLLECTION_NAME.USERS,
-          id
-        ).withConverter(userConverter),
-        data
-      );
-    } else {
-      await updateDoc(
-        doc(db, COLLECTION_NAME.DEFAULT_USERS, id).withConverter(userConverter),
-        data
-      );
-    }
-  } catch (error) {
-    console.log('[DAO] Fail to update doc', error);
-  }
+export async function updateUser(groupId: string, id: string, data: User) {
+  const docRef = getUserRefById(groupId, id);
+
+  await updateDoc(docRef, data);
 }
 
-export async function createUser(data: Omit<User, 'id'>, groupId?: string) {
-  try {
-    await addDoc(
-      collection(
-        db,
-        COLLECTION_NAME.GROUPS,
-        groupId ? groupId : 'default',
-        COLLECTION_NAME.USERS
-      ),
-      data
-    );
-  } catch (error) {
-    console.log('[DAO] Fail to create doc', error);
-  }
+export async function createUser(groupId: string, data: User) {
+  const docRef = await addDoc(getUsersRef(groupId), data);
+
+  return docRef;
 }
 
-export async function deleteUser(id: string, groupId?: string) {
-  try {
-    if (groupId) {
-      await deleteDoc(
-        doc(db, COLLECTION_NAME.GROUPS, groupId, COLLECTION_NAME.USERS, id)
-      );
-    } else {
-      await deleteDoc(doc(db, COLLECTION_NAME.DEFAULT_USERS, id));
-    }
-  } catch (error) {
-    console.log('[DAO] Fail to delete doc', error);
-  }
+export async function deleteUser(groupId: string, id: string) {
+  await deleteDoc(getUserRefById(groupId, id));
 }
