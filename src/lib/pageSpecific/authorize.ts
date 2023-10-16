1a1348d15a87135fd5825fe5e90f501f100cfd54
HenNguyen <hennv404@gmail.com>
2023-10-16 11:22:30 +0700

    seperate storage page

--- src/lib/pageSpecific/authorize.ts
+++ src/lib/pageSpecific/authorize.ts
@@ -8,15 +8,3 @@ export interface AuthorizeContextType {
 export const AuthorizeContext = createContext<AuthorizeContextType>({
   permissions: [],
 });
-
-export const permissionRouteMap = new Map([
-  ['KHO', '/manager/storage'],
-  ['PQ', '/manager/authorize'],
-  ['ĐH', '/manager/orders'],
-  ['KH', '/manager/customers'],
-  ['BC', '/manager/reports'],
-  ['GH', '/manager/deliveries'],
-  ['LH', '/manager/contacts'],
-  ['KM', '/manager/sales'],
-  ['FB', '/manager/feedbacks'],
-]);
