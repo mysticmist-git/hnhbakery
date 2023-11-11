094d6f4bdad07fdad018c91fefdcfb9ea66feb8a
HenNguyen <hennv404@gmail.com>
2023-11-11 16:11:00 +0700

    update next config file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
   },
-}
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.obj$/,
+        use: 'raw-loader',
+      });
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = withPlugins([withImages], nextConfig);
