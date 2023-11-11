17eb87f6f3e888720e13c2575e4e0ee0d922c521
HenNguyen <hennv404@gmail.com>
2023-11-11 16:17:13 +0700

    Revert "update next config file"
    This reverts commit 094d6f4bdad07fdad018c91fefdcfb9ea66feb8a.

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
-const withPlugins = require('next-compose-plugins');
-const withImages = require('next-images');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.module.rules.push({
-        test: /\.obj$/,
-        use: 'raw-loader',
-      });
-    }
+}
 
-    return config;
-  },
-};
-
-module.exports = withPlugins([withImages], nextConfig);
+module.exports = nextConfig
